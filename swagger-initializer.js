54c1209424d65cfbd0c7586530a46b24f273edf9
timzhang <tim.1.zhang@nokia.com>
2023-06-13 11:47:36 -0400

    Update url

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/zhangxd926/mantis-bug-tracker-rest-api/blob/main/mantis-bug-tracker-rest-api.yaml",
+    url: "https://raw.githubusercontent.com/zhangxd926/mantis-bug-tracker-rest-api/main/mantis-bug-tracker-rest-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
